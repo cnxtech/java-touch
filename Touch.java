9c384375f27de519aafecf7291ad14ee0a5b3688
Edgar Lee <edgarl@netflix.com>
2018-11-29 11:00:40 -0800

    Fix formatting

--- Touch.java
+++ Touch.java
@@ -3,32 +3,32 @@ import java.io.FileOutputStream;
 
 public class Touch {
 
-	public static final String filepath = "file";
+    public static final String filepath = "file";
 
-	public static void main(String[] args) throws Exception {
-    File file = new File(filepath);
+    public static void main(String[] args) throws Exception {
+        File file = new File(filepath);
 
-    System.out.println(file.lastModified());
+        System.out.println(file.lastModified());
 
-    if (args.length == 0 || args[0].equals("bad")) {
-      BadTouch(file);
-    } else {
-      GoodTouch(file);
+        if (args.length == 0 || args[0].equals("bad")) {
+            BadTouch(file);
+        } else {
+            GoodTouch(file);
+        }
+
+        System.out.println(file.lastModified());
     }
 
-    System.out.println(file.lastModified());
-	}
-
-  public static void BadTouch(File file) throws Exception {
-    if (!file.setLastModified(System.currentTimeMillis())) {
-      throw new Exception("Could not update time stamp for " + file);
+    public static void BadTouch(File file) throws Exception {
+        if (!file.setLastModified(System.currentTimeMillis())) {
+            throw new Exception("Could not update time stamp for " + file);
+        }
     }
-  }
 
-  public static void GoodTouch(File file) throws Exception {
-    FileOutputStream fos = new FileOutputStream(file);
-    byte[] empty = new byte[0];
-    fos.write(empty);
-  }
+    public static void GoodTouch(File file) throws Exception {
+        FileOutputStream fos = new FileOutputStream(file);
+        byte[] empty = new byte[0];
+        fos.write(empty);
+    }
 
 }
